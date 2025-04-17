1a8db369a1dfd83684fb8a3b3b0fc9895d2629a7
tSult <sultanatasmia61@gmail.com>
2025-04-17 21:26:49 +0600

    did protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import { PublicLayout } from "@/layouts/public-layout"
+import HomePage from "@/routes/home"
+import AuthenticationLayout from "@/layouts/auth-layout"
+import { SignInPage } from "./routes/sign-in"
+import { SignUpPage } from "./routes/sign-up"
 const App = () => {
   return (
-    <div className='text-blue-500'>
-     App
-      
-    </div>
+    <Router>
+      <Routes>
+        {/* public routes */}
+        <Route element={<PublicLayout/>}>
+         <Route index element={<HomePage/>} />
+        </Route>
+
+        {/* authentication layout */}
+        <Route element={<AuthenticationLayout/>}>
+         <Route path="/signin/*" element={<SignInPage/>} />
+         <Route path="/signup/*" element={<SignUpPage/>} />
+        </Route>
+        {/* protected routes */}
+      </Routes>
+    </Router>
   )
 }
 
--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,18 +1,18 @@
-import { Footer } from "@/components/footer";
+import Footer from "@/components/footer";
 import Header from "@/components/header";
 import AuthHanlder from "@/handlers/auth-handler";
 import { Outlet } from "react-router-dom";
+
 
 export const PublicLayout = () => {
   return (
     <div className="w-full">
       {/* handler to store the user data */}
       <AuthHanlder />
-      <Header />
-
-      <Outlet />
-
-      <Footer />
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+     
     </div>
   );
 };
